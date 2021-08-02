ac7bd7a486452edce7ca0386838b6146a63fa50b
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-08-02 16:18:32 +0100

    create dummy react interface for the lottery smart-contract

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import lottery from './lotteryContract';
 import web3 from './web3';
 
-function  App() {
+function App() {
+  const [ manager, setManager ] = useState('');
+  const [ players, setPlayers ] = useState([]);
+  const [ value, setValue ] = useState('');
+  const [ lotteryBalance, setLotteryBalance ] = useState('');
+  const [ msgNotification, setMessage ] = useState('');
 
-  // const getAddress = async () => {
+  useEffect(()=>{
+
+    const fetchLotteryData = async () => {
+      const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
+
+      setManager(manager);
+      setPlayers(players);
+      setLotteryBalance(balance);
+    }
+
+    fetchLotteryData();
+  }, [manager,players,lotteryBalance]);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const userAccount = await web3.eth.getAccounts();
+
+    setMessage('Waiting on transaction to occurr...');
+
+    await lottery.methods.enter().send({
+      from: userAccount[0],
+      value: web3.utils.toWei(value, 'ether')
+    });
+
+    setMessage('You have successfully entered the pool!!!');
+  }
+
+    // const getAddress = async () => {
   //   console.log(await web3.eth.getAccounts());; 
   // }
 
   // getAddress();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2 className="Lottery-header">Lottery Contract </h2>
+      <div className="lottery-body">
+        <h4>This contract is managed by { manager }</h4>
+        <p>There are currently {players.length} people that have entered into the lottery pool, competing 
+           to win {web3.utils.fromWei(lotteryBalance, 'ether')} ether.</p>
+
+         <hr />
+
+         <form onSubmit={onSubmit}>
+           <h4>You want to try your luck?</h4>
+           <div>
+             <label>Amount of ether to enter</label>
+             <input value={value} name="value" onChange={event => setValue(event.target.value)}/>
+           </div>
+           <button>Enter</button>
+         </form>
+
+         <hr />
+         {msgNotification}
+      </div>
+    </>
   );
 }
 
